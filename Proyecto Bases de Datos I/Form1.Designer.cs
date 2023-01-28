c96cc3a2e9eb564e87deeb2f60365e62a7778314
CamiloR293 <jrodallega@unicauca.edu.co>
2023-01-27 21:34:58 -0500

    starter Commit

---
+++ Proyecto Bases de Datos I/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Proyecto_Bases_de_Datos_I
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabMenu = new System.Windows.Forms.TabPage();
+            this.tabRegistro = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabMenu);
+            this.tabControl.Controls.Add(this.tabRegistro);
+            this.tabControl.Location = new System.Drawing.Point(-1, -1);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(802, 450);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabMenu
+            // 
+            this.tabMenu.Location = new System.Drawing.Point(4, 24);
+            this.tabMenu.Name = "tabMenu";
+            this.tabMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMenu.Size = new System.Drawing.Size(794, 422);
+            this.tabMenu.TabIndex = 0;
+            this.tabMenu.Text = "Menu principal";
+            this.tabMenu.UseVisualStyleBackColor = true;
+            // 
+            // tabRegistro
+            // 
+            this.tabRegistro.Location = new System.Drawing.Point(4, 24);
+            this.tabRegistro.Name = "tabRegistro";
+            this.tabRegistro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRegistro.Size = new System.Drawing.Size(794, 422);
+            this.tabRegistro.TabIndex = 1;
+            this.tabRegistro.Text = "Registros";
+            this.tabRegistro.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl;
+        private TabPage tabMenu;
+        private TabPage tabRegistro;
+    }
+}
